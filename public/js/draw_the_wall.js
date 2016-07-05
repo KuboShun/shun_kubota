5c245f254b6e8cb7fac589db09cf829aff063569
KubotaShun <KubotaShun@KuboShuns-Mac.local>
2016-07-05 22:14:04 +0900

    Change a little day2

--- public/js/draw_the_wall.js
+++ public/js/draw_the_wall.js
@@ -1,38 +1,14 @@
-$(document).ready(function () {
+var paint = "white"
+console.log(paint);
 
-  var paint = "white"
+$(".color").on("click", function () {
+  paint = $(this).css('background-color');
   console.log(paint);
+})
 
-  $(".color").on("click", function () {
-    paint = $(this).css('background-color');
-    console.log(paint);
-  })
-
-  $('.colorbox').on('click', function () {
-    $(this).css("background-color", paint );
-    console.log(paint);
-  });
-
+$('.colorbox').on('click', function () {
+  $(this).css("background-color", paint );
+  console.log(paint);
 });
 
-/*
 
-window.onload = function drawTheWall();
-
- {
-  var paint_color = "white"
-  var color = document.getElementsByClassName("color")
-  
-  console.log(color);
-
-  color.onClick = function(){
-    console.log(paint_color);
-    paint_color = this.css('background');
-  }
-
-  document.getElementsByClassName("colorbox").onClick = function(){
-    console.log(paint_color);
-    this.css('background', paint_color);
-  }
-}
-*/
